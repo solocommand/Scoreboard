624ec8e32b674acf231dbc49f54fd87ccb58d7b2
solocommand <solocommand@d49dd244-d8cf-4422-8ed2-6dad544eaf6a>
2010-10-21 02:57:31 +0000

    initial import

---
+++ trunk/localization.lua
@@ -0,0 +1,29 @@
+﻿local L = LibStub("AceLocale-3.0"):GetLocale("Titan", true)
+
+-- default (EN)
+
+TITAN_POINTS_MENU_TEXT = "TitanPoints"
+TITAN_POINTS_MENU_VALOR = "Valor Points";
+TITAN_POINTS_MENU_JUSTICE = "Justice Points";
+TITAN_POINTS_MENU_CONQUEST = "Conquest Points";
+TITAN_POINTS_MENU_HONOR = "Honor Points";
+
+TITAN_POINTS_MENU_LABELS = "Show Point Labels";
+TITAN_POINTS_MENU_SHORT_LABELS = "Use Short Labels";
+TITAN_POINTS_MENU_MEM = "Show Memory Usage";
+TITAN_POINTS_MENU_ICONS = "Show Point Icons";
+
+TITAN_POINTS_BUTTON_LABEL = "Points: ";
+
+TITAN_POINTS_LABEL_VALOR = "Valor Points: %s ";
+TITAN_POINTS_LABEL_VALOR_SHORT = "VP: %s ";
+TITAN_POINTS_LABEL_JUSTICE = "Justice Points: %s ";
+TITAN_POINTS_LABEL_JUSTICE_SHORT = "JP: %s ";
+TITAN_POINTS_LABEL_CONQUEST = "Conquest Points: %s ";
+TITAN_POINTS_LABEL_CONQUEST_SHORT = "CP: %s ";
+TITAN_POINTS_LABEL_HONOR = "Honor Points: %s ";
+TITAN_POINTS_LABEL_HONOR_SHORT = "HP: %s ";
+TITAN_POINTS_LABEL_SPACER = " %s ";
+
+TITAN_POINTS_TOOLTIP = "Currency Points";
+

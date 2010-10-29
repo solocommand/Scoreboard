96f8e6fc3d8acf96400a2f4cbc986d8651b89e9f
solocommand <solocommand@d49dd244-d8cf-4422-8ed2-6dad544eaf6a>
2010-10-29 12:46:42 +0000

    frFR Localization added, thanks to Sasmira!

--- trunk/localization.lua
+++ trunk/localization.lua
@@ -2,16 +2,16 @@
 
 -- default (EN)
 
-TITAN_POINTS_MENU_TEXT = "TitanPoints"
-TITAN_POINTS_MENU_VALOR = "Valor Points";
-TITAN_POINTS_MENU_JUSTICE = "Justice Points";
 TITAN_POINTS_MENU_CONQUEST = "Conquest Points";
+TITAN_POINTS_MENU_HIDE = "Hide";
 TITAN_POINTS_MENU_HONOR = "Honor Points";
-
-TITAN_POINTS_MENU_LABELS = "Show Point Labels";
-TITAN_POINTS_MENU_SHORT_LABELS = "Use Short Labels";
-TITAN_POINTS_MENU_MEM = "Show Memory Usage";
 TITAN_POINTS_MENU_ICONS = "Show Point Icons";
+TITAN_POINTS_MENU_JUSTICE = "Justice Points";
+TITAN_POINTS_MENU_LABELS = "Show Point Labels";
+TITAN_POINTS_MENU_MEM = "Show Memory Usage";
+TITAN_POINTS_MENU_SHORT_LABELS = "Use Short Labels";
+TITAN_POINTS_MENU_TEXT = "TitanPoints";
+TITAN_POINTS_MENU_VALOR = "Valor Points";
 
 TITAN_POINTS_BUTTON_LABEL = "Points: ";
 
@@ -32,3 +32,39 @@ TITAN_POINTS_HONOR = "Honor Points";
 
 TITAN_POINTS_TOOLTIP = "Currency Points";
 
+-- frFR
+
+if (GetLocale() == "frFR") then
+
+-- Last Update by Sasmira: 10/29/2010
+
+TITAN_POINTS_MENU_CONQUEST = "Points de conquête";
+TITAN_POINTS_MENU_HIDE = "Cacher"; -- Missing in English and don't work in french
+TITAN_POINTS_MENU_HONOR = "Points d'honneur";
+TITAN_POINTS_MENU_ICONS = "Afficher l'icône";
+TITAN_POINTS_MENU_JUSTICE = "Points de justice";
+TITAN_POINTS_MENU_LABELS = "Afficher les noms";
+TITAN_POINTS_MENU_MEM = "Consommation de mémoire";
+TITAN_POINTS_MENU_SHORT_LABELS = "Utiliser les noms courts";
+TITAN_POINTS_MENU_TEXT = "TitanPoints";
+TITAN_POINTS_MENU_VALOR = "Sceau de champion";
+
+TITAN_POINTS_BUTTON_LABEL = "Points: ";
+
+TITAN_POINTS_LABEL_VALOR = "Sceau de champion: %s ";
+TITAN_POINTS_LABEL_VALOR_SHORT = "SC: %s ";
+TITAN_POINTS_LABEL_JUSTICE = "Points de justice: %s ";
+TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
+TITAN_POINTS_LABEL_CONQUEST = "Points de conquête: %s ";
+TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
+TITAN_POINTS_LABEL_HONOR = "Points d'honneur: %s ";
+TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
+
+TITAN_POINTS_VALOR = "Sceau de champion";
+TITAN_POINTS_JUSTICE = "Points de justice";
+TITAN_POINTS_CONQUEST = "Points de conquête";
+TITAN_POINTS_HONOR = "Points d'honneur";
+
+TITAN_POINTS_TOOLTIP = "Monnaies";
+	
+end

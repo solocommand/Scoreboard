3b5ba2b49ebd68bfb0ac45f06e3512b25eb08592
solocommand <solocommand@d49dd244-d8cf-4422-8ed2-6dad544eaf6a>
2010-10-30 14:28:42 +0000

    deDE fixes and esES localization

--- trunk/localization.lua
+++ trunk/localization.lua
@@ -25,7 +25,6 @@ TITAN_POINTS_LABEL_HONOR = "Honor Points: %s ";
 TITAN_POINTS_LABEL_HONOR_SHORT = "HP: %s ";
 TITAN_POINTS_LABEL_SPACER = " %s ";
 
-
 TITAN_POINTS_VALOR = "Valor Points";
 TITAN_POINTS_JUSTICE = "Justice Points";
 TITAN_POINTS_CONQUEST = "Conquest Points";
@@ -78,39 +77,82 @@ end
 
 if (GetLocale() == "deDE") then
 
-	-- Last Update by solocommand: 10/29/2010
+	-- Last Update by solocommand: 10/30/2010
 
-	TITAN_POINTS_MENU_CONQUEST = "Eroberungspunkte";
+	TITAN_POINTS_MENU_CONQUEST = "Zeige Eroberungspunkte";
 	TITAN_POINTS_MENU_HIDE = "Ausblenden";
-	--TITAN_POINTS_MENU_HONOR = "Honor Points";
+	TITAN_POINTS_MENU_HONOR = "Zeige Ehrenpunkte";
 	--TITAN_POINTS_MENU_ICONS = "Show Point Icons";
-	--TITAN_POINTS_MENU_JUSTICE = "Justice Points";
+	TITAN_POINTS_MENU_JUSTICE = "Zeige Gerechtigkeitspunkte";
 	--TITAN_POINTS_MENU_LABELS = "Show Point Labels";
 	TITAN_POINTS_MENU_MEM = "Zeige Spiecher Verbrauch";
 	--TITAN_POINTS_MENU_SHORT_LABELS = "Use Short Labels";
-	--TITAN_POINTS_MENU_TEXT = "TitanPoints";
-	--TITAN_POINTS_MENU_VALOR = "Valor Points";
+	TITAN_POINTS_MENU_TEXT = "TitanPoints";
+	TITAN_POINTS_MENU_VALOR = "Zeige Tapferkeitspunkte";
 
-	--TITAN_POINTS_BUTTON_LABEL = "Points: ";
+	TITAN_POINTS_BUTTON_LABEL = "Punkte: ";
 
-	--TITAN_POINTS_LABEL_VALOR = "Valor Points: %s ";
-	--TITAN_POINTS_LABEL_VALOR_SHORT = "VP: %s ";
-	--TITAN_POINTS_LABEL_JUSTICE = "Justice Points: %s ";
-	--TITAN_POINTS_LABEL_JUSTICE_SHORT = "JP: %s ";
+	TITAN_POINTS_LABEL_VALOR = "Tapferkeitspunkte: %s ";
+	TITAN_POINTS_LABEL_VALOR_SHORT = "TP: %s ";
+	TITAN_POINTS_LABEL_JUSTICE = "Gerechtigkeitspunkte: %s ";
+	TITAN_POINTS_LABEL_JUSTICE_SHORT = "GP: %s ";
 	TITAN_POINTS_LABEL_CONQUEST = "Eroberungspunkte: %s ";
-	--TITAN_POINTS_LABEL_CONQUEST_SHORT = "CP: %s ";
-	--TITAN_POINTS_LABEL_HONOR = "Honor Points: %s ";
-	--TITAN_POINTS_LABEL_HONOR_SHORT = "HP: %s ";
-	--TITAN_POINTS_LABEL_SPACER = " %s ";
+	TITAN_POINTS_LABEL_CONQUEST_SHORT = "ErP: %s ";
+	TITAN_POINTS_LABEL_HONOR = "Ehrenpunkte: %s ";
+	ITAN_POINTS_LABEL_HONOR_SHORT = "EhP: %s ";
 
-
-	--TITAN_POINTS_VALOR = "Valor Points";
-	--TITAN_POINTS_JUSTICE = "Justice Points";
+	TITAN_POINTS_VALOR = "Tapferkeitspunkte";
+	TITAN_POINTS_JUSTICE = "Gerechtigkeitspunkte";
 	TITAN_POINTS_CONQUEST = "Eroberungspunkte";
-	--TITAN_POINTS_HONOR = "Honor Points";
+	TITAN_POINTS_HONOR = "Ehrenpunkte";
 
-	--TITAN_POINTS_TOOLTIP = "Currency Points";
+	TITAN_POINTS_TOOLTIP = "Währungen Punkte";
 	TITAN_POINTS_TOOLTIP_MEM = "Speicher Verbrauch:";
 	TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB";
+	
+end
+
+-- esES
+
+if (GetLocale() == "esES") then
+
+	-- Last Update by solocommand: 10/30/2010
+
+	TITAN_POINTS_MENU_CONQUEST = "Mostrar puntos de conquista";
+	TITAN_POINTS_MENU_HIDE = "Ocultar";
+	TITAN_POINTS_MENU_HONOR = "Mostrar puntos de valor";
+	TITAN_POINTS_MENU_ICONS = "Mostrar icono de puntos";
+	TITAN_POINTS_MENU_JUSTICE = "Mostrar puntos de justicia";
+	TITAN_POINTS_MENU_LABELS = "Mostrar etiqueta de puntos";
+	TITAN_POINTS_MENU_MEM = "Mostrar uso de memoria";
+	TITAN_POINTS_MENU_SHORT_LABELS = "Mostrar pequeño etiqueta";
+	TITAN_POINTS_MENU_TEXT = "TitanPoints";
+	TITAN_POINTS_MENU_VALOR = "Mostrar puntos de valor";
+
+	TITAN_POINTS_BUTTON_LABEL = "Puntos: ";
+
+	TITAN_POINTS_LABEL_VALOR = "Puntos de valor: %s ";
+	TITAN_POINTS_LABEL_VALOR_SHORT = "PV: %s ";
+	TITAN_POINTS_LABEL_JUSTICE = "Puntos de justicia: %s ";
+	TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
+	TITAN_POINTS_LABEL_CONQUEST = "Puntos de conquista: %s ";
+	TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
+	TITAN_POINTS_LABEL_HONOR = "Puntos de honor: %s ";
+	TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
+
+	TITAN_POINTS_VALOR = "Puntos de valor";
+	TITAN_POINTS_JUSTICE = "Puntos de justicia";
+	TITAN_POINTS_CONQUEST = "Puntos de conquista";
+	TITAN_POINTS_HONOR = "Puntos de honor";
+
+	TITAN_POINTS_TOOLTIP = "Puntos de Monedas";
+	TITAN_POINTS_TOOLTIP_MEM = "Uso de Memoria:";
+	TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB";
+	
+end
+
+-- ruRU
+
+if (GetLocale() == "ruRU") then
 	
 end

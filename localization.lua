0045a433be7ccb59f07a7ace1de8d9a444135020
Joshua Worden <josh.worden@iwmentertainment.com>
2015-01-01 22:49:02 -0600

    Update localization.lua
    Updated deDE and frFR localizations from mundl78 and Alena. Added
    support for localization of 'Seals of Tempered Fate'

--- localization.lua
+++ localization.lua
@@ -2,6 +2,11 @@
 
 -- default (EN)
 
+TITAN_POINTS_SEAL_OF_TEMPERED_FATE = "Seal of Tempered Fate";
+TITAN_POINTS_MENU_SEAL_OF_TEMPERED_FATE = "Seal of Tempered Fate";
+TITAN_POINTS_LABEL_SEAL_OF_TEMPERED_FATE = "Seals of Tempered Fate: %s";
+TITAN_POINTS_LABEL_SEAL_OF_TEMPERED_FATE_SHORT = "STF: %s";
+
 TITAN_POINTS_MENU_CONQUEST = "Conquest Points";
 TITAN_POINTS_MENU_HIDE = "Hide";
 TITAN_POINTS_MENU_HKS = "Honor Kills";
@@ -48,44 +53,55 @@ TITAN_POINTS_TOOLTIP_MEM_UNIT = "Kb";
 
 if (GetLocale() == "frFR") then
 
-	-- Last Update by Sasmira: 12/20/2010
+    -- Last Update by Sasmira: 12/20/2010
 
-	TITAN_POINTS_MENU_CONQUEST = "Points de conquête";
-	TITAN_POINTS_MENU_HIDE = "Cacher";
-	TITAN_POINTS_MENU_HKS = "Victoires Honorables";
-	TITAN_POINTS_MENU_HONOR = "Points d'honneur";
-	TITAN_POINTS_MENU_ICONS = "Afficher l'icône";
-	TITAN_POINTS_MENU_JUSTICE = "Points de justice";
-	TITAN_POINTS_MENU_LABELS = "Afficher les noms";
-	TITAN_POINTS_MENU_MEM = "Consommation de mémoire";
-	TITAN_POINTS_MENU_SHORT_LABELS = "Utiliser les noms courts";
-	TITAN_POINTS_MENU_TEXT = "TitanPoints";
-	TITAN_POINTS_MENU_VALOR = "Points de vaillance";
-	TITAN_POINTS_MENU_GARRISON = "Garrison Resources";
+    TITAN_POINTS_MENU_CONQUEST = "Points de conquête";
+    TITAN_POINTS_MENU_HIDE = "Cacher";
+    TITAN_POINTS_MENU_HKS = "Victoires Honorables";
+    TITAN_POINTS_MENU_HONOR = "Points d'honneur";
+    TITAN_POINTS_MENU_ICONS = "Afficher l'icône";
+    TITAN_POINTS_MENU_JUSTICE = "Points de justice";
+    TITAN_POINTS_MENU_LABELS = "Afficher les noms";
+    TITAN_POINTS_MENU_MEM = "Consommation de mémoire";
+    TITAN_POINTS_MENU_SHORT_LABELS = "Utiliser les noms courts";
+    TITAN_POINTS_MENU_TEXT = "TitanPoints";
+    TITAN_POINTS_MENU_VALOR = "Points de vaillance";
+    TITAN_POINTS_MENU_TIMELESS = "Sceaux du destin trempé";
+    TITAN_POINTS_MENU_APEXIS = "Cristaux apogides";
+    TITAN_POINTS_MENU_GARRISON = "Ressources de fief"; 
 
-	TITAN_POINTS_BUTTON_LABEL = "Points: ";
+    TITAN_POINTS_BUTTON_LABEL = "Points: ";
 
-	TITAN_POINTS_LABEL_VALOR = "Points de vaillance: %s ";
-	TITAN_POINTS_LABEL_VALOR_SHORT = "PV: %s ";
-	TITAN_POINTS_LABEL_JUSTICE = "Points de justice: %s ";
-	TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
-	TITAN_POINTS_LABEL_CONQUEST = "Points de conquête: %s ";
-	TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
-	TITAN_POINTS_LABEL_HONOR = "Points d'honneur: %s ";
-	TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
-	TITAN_POINTS_LABEL_HKS = "Victoires Honorables: %s ";
-	TITAN_POINTS_LABEL_HKS_SHORT = "VH: %s ";
+    TITAN_POINTS_LABEL_APEXIS = "Cristaux apogides: %s ";
+    TITAN_POINTS_LABEL_APEXIS_SHORT = "CA: %s ";
+    TITAN_POINTS_LABEL_GARRISON = "Ressources de fief: %s ";
+    TITAN_POINTS_LABEL_GARRISON_SHORT = "RF: %s ";
+    TITAN_POINTS_LABEL_TIMELESS = "Sceaux du destin trempé: %s ";
+    TITAN_POINTS_LABEL_TIMELESS_SHORT = "SD: %s ";
+    TITAN_POINTS_LABEL_VALOR = "Points de vaillance: %s ";
+    TITAN_POINTS_LABEL_VALOR_SHORT = "PV: %s ";
+    TITAN_POINTS_LABEL_JUSTICE = "Points de justice: %s ";
+    TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
+    TITAN_POINTS_LABEL_CONQUEST = "Points de conquête: %s ";
+    TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
+    TITAN_POINTS_LABEL_HONOR = "Points d'honneur: %s ";
+    TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
+    TITAN_POINTS_LABEL_HKS = "Victoires Honorables: %s ";
+    TITAN_POINTS_LABEL_HKS_SHORT = "VH: %s ";
 
-	TITAN_POINTS_VALOR = "Points de vaillance";
-	TITAN_POINTS_JUSTICE = "Points de justice";
-	TITAN_POINTS_CONQUEST = "Points de conquête";
-	TITAN_POINTS_HONOR = "Points d'honneur";
-	TITAN_POINTS_HKS = "Victoires Honorables";
+    TITAN_POINTS_APEXIS = "Cristal apogide";
+    TITAN_POINTS_GARRISON = "Ressources de fief";
+    TITAN_POINTS_TIMELESS = "Sceau du destin trempé"; 
+    TITAN_POINTS_VALOR = "Points de vaillance";
+    TITAN_POINTS_JUSTICE = "Points de justice";
+    TITAN_POINTS_CONQUEST = "Points de conquête";
+    TITAN_POINTS_HONOR = "Points d'honneur";
+    TITAN_POINTS_HKS = "Victoires Honorables";
 
-	TITAN_POINTS_TOOLTIP = "Monnaies";
-	TITAN_POINTS_TOOLTIP_MEM = "Mémoire utilis\195\169e:";
-	TITAN_POINTS_TOOLTIP_MEM_UNIT = "Ko";
-	
+    TITAN_POINTS_TOOLTIP = "Monnaies";
+    TITAN_POINTS_TOOLTIP_MEM = "Mémoire utilis\195\169e:";
+    TITAN_POINTS_TOOLTIP_MEM_UNIT = "Ko";
+    
 end
 
 
@@ -93,92 +109,93 @@ end
 
 if (GetLocale() == "deDE") then
 
-	-- Last Update by solocommand: 10/30/2010
+    -- Last Update by solocommand: 10/30/2010
 
-	TITAN_POINTS_MENU_CONQUEST = "Zeige Eroberungspunkte";
-	TITAN_POINTS_MENU_HIDE = "Ausblenden";
-	--TITAN_POINTS_MENU_HKS = "Honor Kills";
-	TITAN_POINTS_MENU_HONOR = "Zeige Ehrenpunkte";
-	--TITAN_POINTS_MENU_ICONS = "Show Point Icons";
-	TITAN_POINTS_MENU_JUSTICE = "Zeige Gerechtigkeitspunkte";
-	--TITAN_POINTS_MENU_LABELS = "Show Point Labels";
-	TITAN_POINTS_MENU_MEM = "Zeige Spiecher Verbrauch";
-	--TITAN_POINTS_MENU_SHORT_LABELS = "Use Short Labels";
-	TITAN_POINTS_MENU_TEXT = "TitanPoints";
-	TITAN_POINTS_MENU_VALOR = "Zeige Tapferkeitspunkte";
-	TITAN_POINTS_MENU_GARRISON = "Garrison Resources";
+    TITAN_POINTS_MENU_CONQUEST = "Zeige Eroberungspunkte";
+    TITAN_POINTS_MENU_HIDE = "Ausblenden";
+    TITAN_POINTS_MENU_HKS = "Zeige Ehrenhafte Siege";
+    TITAN_POINTS_MENU_HONOR = "Zeige Ehrenpunkte";
+    TITAN_POINTS_MENU_ICONS = "Zeige Symbole";
+    TITAN_POINTS_MENU_LABELS = "Zeige Beschriftung";
+    TITAN_POINTS_MENU_MEM = "Zeige Spiecher Verbrauch";
+    TITAN_POINTS_MENU_SHORT_LABELS = "Benutze Abkürzungen";
+    TITAN_POINTS_MENU_TEXT = "TitanPoints";
+    TITAN_POINTS_MENU_APEXIS = "Zeige Apexiskristalle";
+    TITAN_POINTS_MENU_GARRISON = "Zeige Garnisonsressourcen";
+    TITAN_POINTS_MENU_TIMELESS = "Zeige Zeitlose Münzen";
+    
+    TITAN_POINTS_BUTTON_LABEL = "Punkte: ";
 
-	TITAN_POINTS_BUTTON_LABEL = "Punkte: ";
+    
+    TITAN_POINTS_LABEL_APEXIS = "Apexiskristalle: %s ";
+    TITAN_POINTS_LABEL_APEXIS_SHORT = "AK: %s ";
+    TITAN_POINTS_LABEL_GARRISON = "Garnisonsressourcen: %s ";
+    TITAN_POINTS_LABEL_GARRISON_SHORT = "GR: %s ";
+    TITAN_POINTS_LABEL_CONQUEST = "Eroberungspunkte: %s ";
+    TITAN_POINTS_LABEL_CONQUEST_SHORT = "ErP: %s ";
+    TITAN_POINTS_LABEL_HONOR = "Ehrenpunkte: %s ";
+    TITAN_POINTS_LABEL_HONOR_SHORT = "EhP: %s ";
+    TITAN_POINTS_LABEL_HKS = "Ehrenhafte Siege: %s ";
+    TITAN_POINTS_LABEL_HKS_SHORT = "ES: %s ";
+    TITAN_POINTS_LABEL_TIMELESS = "Zeitlose Münzen: %s ";
+    TITAN_POINTS_LABEL_TIMELESS_SHORT = "ZM: %s ";
 
-	TITAN_POINTS_LABEL_VALOR = "Tapferkeitspunkte: %s ";
-	TITAN_POINTS_LABEL_VALOR_SHORT = "TP: %s ";
-	TITAN_POINTS_LABEL_JUSTICE = "Gerechtigkeitspunkte: %s ";
-	TITAN_POINTS_LABEL_JUSTICE_SHORT = "GP: %s ";
-	TITAN_POINTS_LABEL_CONQUEST = "Eroberungspunkte: %s ";
-	TITAN_POINTS_LABEL_CONQUEST_SHORT = "ErP: %s ";
-	TITAN_POINTS_LABEL_HONOR = "Ehrenpunkte: %s ";
-	ITAN_POINTS_LABEL_HONOR_SHORT = "EhP: %s ";
-	--TITAN_POINTS_LABEL_HKS = "Honor Kills: %s ";
-	--TITAN_POINTS_LABEL_HKS_SHORT = "HKs: %s ";
+    TITAN_POINTS_APEXIS = "Apexiskristall";
+    TITAN_POINTS_GARRISON = "Garnisonsressourcen";
+    TITAN_POINTS_TIMELESS = "Zeitlose Münze";
+    TITAN_POINTS_CONQUEST = "Eroberungspunkte";
+    TITAN_POINTS_HONOR = "Ehrenpunkte";
+    TITAN_POINTS_HKS = "Ehrenhafte Siege";
 
-	TITAN_POINTS_VALOR = "Tapferkeitspunkte";
-	TITAN_POINTS_JUSTICE = "Gerechtigkeitspunkte";
-	TITAN_POINTS_CONQUEST = "Eroberungspunkte";
-	TITAN_POINTS_HONOR = "Ehrenpunkte";
-	--TITAN_POINTS_HKS = "Honor Kills";
-
-	TITAN_POINTS_TOOLTIP = "Währungen Punkte";
-	TITAN_POINTS_TOOLTIP_MEM = "Speicher Verbrauch:";
-	TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB";
-	
+    TITAN_POINTS_TOOLTIP = "Währungen Punkte";
+    TITAN_POINTS_TOOLTIP_MEM = "Speicher Verbrauch:";
+    TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB"; 
+    
 end
 
 -- esES
 
 if (GetLocale() == "esES") then
 
-	-- Last Update by solocommand: 10/30/2010
+    -- Last Update by solocommand: 10/30/2010
 
-	TITAN_POINTS_MENU_CONQUEST = "Mostrar puntos de conquista";
-	TITAN_POINTS_MENU_HIDE = "Ocultar";
-	--TITAN_POINTS_MENU_HKS = "Honor Kills";
-	TITAN_POINTS_MENU_HONOR = "Mostrar puntos de valor";
-	TITAN_POINTS_MENU_ICONS = "Mostrar icono de puntos";
-	TITAN_POINTS_MENU_JUSTICE = "Mostrar puntos de justicia";
-	TITAN_POINTS_MENU_LABELS = "Mostrar etiqueta de puntos";
-	TITAN_POINTS_MENU_MEM = "Mostrar uso de memoria";
-	TITAN_POINTS_MENU_SHORT_LABELS = "Mostrar pequeño etiqueta";
-	TITAN_POINTS_MENU_TEXT = "TitanPoints";
-	TITAN_POINTS_MENU_VALOR = "Mostrar puntos de valor";
-	TITAN_POINTS_MENU_GARRISON = "Garrison Resources";
+    TITAN_POINTS_MENU_CONQUEST = "Mostrar puntos de conquista";
+    TITAN_POINTS_MENU_HIDE = "Ocultar";
+    --TITAN_POINTS_MENU_HKS = "Honor Kills";
+    TITAN_POINTS_MENU_HONOR = "Mostrar puntos de valor";
+    TITAN_POINTS_MENU_ICONS = "Mostrar icono de puntos";
+    TITAN_POINTS_MENU_JUSTICE = "Mostrar puntos de justicia";
+    TITAN_POINTS_MENU_LABELS = "Mostrar etiqueta de puntos";
+    TITAN_POINTS_MENU_MEM = "Mostrar uso de memoria";
+    TITAN_POINTS_MENU_SHORT_LABELS = "Mostrar pequeño etiqueta";
+    TITAN_POINTS_MENU_TEXT = "TitanPoints";
+    TITAN_POINTS_MENU_VALOR = "Mostrar puntos de valor";
+    TITAN_POINTS_MENU_GARRISON = "Garrison Resources";
 
-	TITAN_POINTS_BUTTON_LABEL = "Puntos: ";
+    TITAN_POINTS_BUTTON_LABEL = "Puntos: ";
 
-	TITAN_POINTS_LABEL_VALOR = "Puntos de valor: %s ";
-	TITAN_POINTS_LABEL_VALOR_SHORT = "PV: %s ";
-	TITAN_POINTS_LABEL_JUSTICE = "Puntos de justicia: %s ";
-	TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
-	TITAN_POINTS_LABEL_CONQUEST = "Puntos de conquista: %s ";
-	TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
-	TITAN_POINTS_LABEL_HONOR = "Puntos de honor: %s ";
-	TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
-	--TITAN_POINTS_LABEL_HKS = "Honor Kills: %s ";
-	--TITAN_POINTS_LABEL_HKS_SHORT = "HKs: %s ";
+    TITAN_POINTS_LABEL_VALOR = "Puntos de valor: %s ";
+    TITAN_POINTS_LABEL_VALOR_SHORT = "PV: %s ";
+    TITAN_POINTS_LABEL_JUSTICE = "Puntos de justicia: %s ";
+    TITAN_POINTS_LABEL_JUSTICE_SHORT = "PJ: %s ";
+    TITAN_POINTS_LABEL_CONQUEST = "Puntos de conquista: %s ";
+    TITAN_POINTS_LABEL_CONQUEST_SHORT = "PC: %s ";
+    TITAN_POINTS_LABEL_HONOR = "Puntos de honor: %s ";
+    TITAN_POINTS_LABEL_HONOR_SHORT = "PH: %s ";
 
-	TITAN_POINTS_VALOR = "Puntos de valor";
-	TITAN_POINTS_JUSTICE = "Puntos de justicia";
-	TITAN_POINTS_CONQUEST = "Puntos de conquista";
-	TITAN_POINTS_HONOR = "Puntos de honor";
-	--TITAN_POINTS_HKS = "Honor Kills";
+    TITAN_POINTS_VALOR = "Puntos de valor";
+    TITAN_POINTS_JUSTICE = "Puntos de justicia";
+    TITAN_POINTS_CONQUEST = "Puntos de conquista";
+    TITAN_POINTS_HONOR = "Puntos de honor";
 
-	TITAN_POINTS_TOOLTIP = "Puntos de Monedas";
-	TITAN_POINTS_TOOLTIP_MEM = "Uso de Memoria:";
-	TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB";
-	
+    TITAN_POINTS_TOOLTIP = "Puntos de Monedas";
+    TITAN_POINTS_TOOLTIP_MEM = "Uso de Memoria:";
+    TITAN_POINTS_TOOLTIP_MEM_UNIT = "KB";
+    
 end
 
 -- ruRU
 
 if (GetLocale() == "ruRU") then
-	
+    
 end

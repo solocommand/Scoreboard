4c743430c96422f181bb28eebdb2530e42d4126f
Joshua Worden <solocommand@gmail.com>
2016-10-25 18:40:24 -0500

    Version bump

--- TitanPoints.lua
+++ TitanPoints.lua
@@ -6,7 +6,7 @@
 
 local bDebugMode = false;
 TITAN_POINTS_ID = "Points";
-TITAN_POINTS_VERSION = "7.0.3";
+TITAN_POINTS_VERSION = "7.1.0";
 TITAN_NIL = false;
 TITAN_POINTS_TAB = "TokenFrame";    -- Currency Tab
 
